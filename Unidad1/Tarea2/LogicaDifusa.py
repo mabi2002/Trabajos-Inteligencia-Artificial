a5780b7ea717056dc22b55ac1696aff8dcd06723
Miguel Angel Barraza León <161258544+mabi2002@users.noreply.github.com>
2025-05-21 20:17:57 -0700

    Add files via upload

--- Unidad1/Tarea2/LogicaDifusa.py
+++ Unidad1/Tarea2/LogicaDifusa.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+# Cargar el CSV
+df = pd.read_csv("C:/Users/Migue/OneDrive/Escritorio/IndicadoresSalud.csv", sep=";", usecols=["Country"])
+
+# Guardar solo la columna de países en un nuevo archivo CSV
+df.to_csv("paises.csv", index=False)
+
+print("Archivo guardado con éxito 🎉")
