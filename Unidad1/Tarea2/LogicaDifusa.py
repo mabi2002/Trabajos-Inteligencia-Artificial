4873476aa4662995cd6cee7e98cff94c6998a1df
Miguel Angel Barraza León <161258544+mabi2002@users.noreply.github.com>
2025-05-21 20:51:59 -0700

    Add files via upload

--- Unidad1/Tarea2/LogicaDifusa.py
+++ Unidad1/Tarea2/LogicaDifusa.py
@@ -1,9 +1,38 @@
-import pandas as pd
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
 
-# Cargar el CSV
-df = pd.read_csv("C:/Users/Migue/OneDrive/Escritorio/IndicadoresSalud.csv", sep=";", usecols=["Country"])
+# Crear variables difusas
+calidad = ctrl.Antecedent(np.arange(0, 11, 1), 'calidad')
+servicio = ctrl.Antecedent(np.arange(0, 11, 1), 'servicio')
+propina = ctrl.Consequent(np.arange(0, 26, 1), 'propina')
 
-# Guardar solo la columna de países en un nuevo archivo CSV
-df.to_csv("paises.csv", index=False)
+# Definir funciones de membresía manualmente en español
+calidad['mala'] = fuzz.trimf(calidad.universe, [0, 0, 5])
+calidad['regular'] = fuzz.trimf(calidad.universe, [0, 5, 10])
+calidad['buena'] = fuzz.trimf(calidad.universe, [5, 10, 10])
 
-print("Archivo guardado con éxito 🎉")
+servicio['malo'] = fuzz.trimf(servicio.universe, [0, 0, 5])
+servicio['normal'] = fuzz.trimf(servicio.universe, [0, 5, 10])
+servicio['bueno'] = fuzz.trimf(servicio.universe, [5, 10, 10])
+
+propina['baja'] = fuzz.trimf(propina.universe, [0, 0, 13])
+propina['media'] = fuzz.trimf(propina.universe, [0, 13, 25])
+propina['alta'] = fuzz.trimf(propina.universe, [13, 25, 25])
+
+# Reglas difusas
+regla1 = ctrl.Rule(calidad['mala'] | servicio['malo'], propina['baja'])
+regla2 = ctrl.Rule(servicio['normal'], propina['media'])
+regla3 = ctrl.Rule(servicio['bueno'] | calidad['buena'], propina['alta'])
+
+# Sistema de control
+sistema = ctrl.ControlSystem([regla1, regla2, regla3])
+simulacion = ctrl.ControlSystemSimulation(sistema)
+
+# Entradas
+simulacion.input['calidad'] = 6.5
+simulacion.input['servicio'] = 9.8
+
+# Calcular
+simulacion.compute()
+print(f"Propina sugerida: {simulacion.output['propina']:.2f}%")
